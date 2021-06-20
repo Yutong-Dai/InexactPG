8007c5573a0e4e472ae7e26ce47aac3b0d425c0c
Roth <rothdyt@gmail.com>
2021-06-20 10:30:31 -0400

    add bacth test

--- IPG/test/natOG/demo.py
+++ IPG/test/natOG/demo.py
@@ -20,7 +20,7 @@ from src.natOG.Solver import Solver
 
 
 loss = 'logit'
-datasetName = 'colon_cancer'
+datasetName = 'leu'
 fileType = fileTypeDict[datasetName]
 print("Working on: {}...".format(datasetName))
 X, y = utils.set_up_xy(datasetName, fileType, dbDir='../../../../GroupFaRSA/db')
